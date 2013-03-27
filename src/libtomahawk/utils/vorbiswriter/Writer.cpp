30481b3ac041a7f80213e08768621e8d31ca0675
AltarBeastiful <r3m1.benoit@gmail.com>
2013-03-27 01:03:16 +0100

    Added Ogg/Vorbis conversion and output of the AudioDataOuput.

---
+++ src/libtomahawk/utils/vorbiswriter/Writer.cpp
@@ -0,0 +1,78 @@
+/*
+	SkypeRec
+	Copyright 2008-2009 by jlh <jlh@gmx.ch>
+	Copyright 2010-2011 by Peter Savichev  (proton) <psavichev@gmail.com>
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation; either version 2 of the License, version 3 of
+	the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+	The GNU General Public License version 2 is included with the source of
+	this program under the file name COPYING.  You can also get a copy on
+	http://www.fsf.org/
+*/
+
+#include <QFileInfo>
+#include <QDir>
+
+#include "utils/Logger.h"
+
+#include "Writer.h"
+
+AudioFileWriter::AudioFileWriter() :
+	sampleRate(0),
+	stereo(false),
+	samplesWritten(0),
+	mustWriteTags(true)
+{
+}
+
+AudioFileWriter::~AudioFileWriter() {
+	if (file.isOpen()) {
+        tDebug()  << "WARNING: AudioFileWriter::~AudioFileWriter(): File has not been closed, closing it now";
+		close();
+	}
+}
+
+void AudioFileWriter::setTags(const QString &comment, const QDateTime &t)
+{
+	tagComment = comment;
+	tagTime = t;
+	mustWriteTags = true;
+}
+
+bool AudioFileWriter::open(const QString &fn, long sr, bool s)
+{
+    bool b = QDir().mkpath(QFileInfo(fn).path());
+	if (!b)
+		return false;
+
+	file.setFileName(fn);
+	sampleRate = sr;
+	stereo = s;
+
+    tDebug()  << QString("Opening '%1'").arg(file.fileName());
+
+	return file.open(QIODevice::WriteOnly);
+}
+
+void AudioFileWriter::close() {
+	if (!file.isOpen()) {
+        tDebug()  << "WARNING: AudioFileWriter::close() called, but file not open";
+		return;
+	}
+
+    tDebug()  << QString("Closing '%1', wrote %2 samples, %3 seconds").arg(file.fileName()).arg(samplesWritten).arg(samplesWritten / sampleRate);
+	return file.close();
+}
+
