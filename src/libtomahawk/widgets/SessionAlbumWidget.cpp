aa8fb848d44f6da7f63ff2dfc8cee94353d40e61
GuillaumeJ <guillaume.jandin1@gmail.com>
2013-04-14 17:36:50 +0200

    modified sessionalbumwidget

--- src/libtomahawk/widgets/SessionAlbumWidget.cpp
+++ src/libtomahawk/widgets/SessionAlbumWidget.cpp
@@ -1,11 +1,38 @@
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2012, Casey Link <unnamedrambler@gmail.com>
+ *   Copyright 2010-2011, Christian Muehlhaeuser <muesli@tomahawk-player.org>
+ *   Copyright 2011, Leo Franchi <lfranchi@kde.org>
+ *   Copyright 2011, Jeff Mitchell <jeff@tomahawk-player.org>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "SessionAlbumWidget.h"
 #include "ui_SessionAlbumWidget.h"
+#include "playlist/RecentlyPlayedModel.h"
 
 SessionAlbumWidget::SessionAlbumWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SessionAlbumWidget)
 {
     ui->setupUi(this);
+//    m_tracksModel = new RecentlyPlayedModel( ui->listView );
+//    ui->listView->proxyModel()->setStyle( PlayableProxyModel::ShortWithAvatars );
+//    ui->listView->overlay()->setEnabled( false );
+//    ui->listView->setPlaylistModel( m_tracksModel );
+//    m_tracksModel->setSource( source_ptr() );
 }
 
 SessionAlbumWidget::~SessionAlbumWidget()
--- src/libtomahawk/widgets/SessionAlbumWidget.h
+++ src/libtomahawk/widgets/SessionAlbumWidget.h
@@ -1,11 +1,35 @@
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2012, Casey Link <unnamedrambler@gmail.com>
+ *   Copyright 2010-2011, Christian Muehlhaeuser <muesli@tomahawk-player.org>
+ *   Copyright 2011, Leo Franchi <lfranchi@kde.org>
+ *   Copyright 2011, Jeff Mitchell <jeff@tomahawk-player.org>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SESSIONALBUMWIDGET_H
 #define SESSIONALBUMWIDGET_H
 
 #include <QWidget>
 #include "PlaylistInterface.h"
 #include "ViewPage.h"
+
+class RecentlyPlayedModel;
+
 namespace Ui {
-class SessionAlbumWidget;
+    class SessionAlbumWidget;
 }
 
 namespace Tomahawk
@@ -15,23 +39,6 @@ namespace Tomahawk
     class ChartDataLoader;
 }
 
-//class DLLEXPORT PlaylistWidget : public QListView
-//{
-//Q_OBJECT
-
-//public:
-//    PlaylistWidget( QWidget* parent = 0 );
-
-//    OverlayWidget* overlay() const { return m_overlay; }
-
-//    virtual void setModel( QAbstractItemModel* model );
-
-//signals:
-//    void modelChanged();
-
-//private:
-//    OverlayWidget* m_overlay;
-//};
 
 class SessionAlbumWidget : public QWidget, public Tomahawk::ViewPage
 {
@@ -57,6 +64,7 @@ public:
 private:
     Ui::SessionAlbumWidget *ui;
     Tomahawk::playlistinterface_ptr m_playlistInterface;
+    RecentlyPlayedModel* m_tracksModel;
 };
 
 #endif // SESSIONALBUMWIDGET_H
