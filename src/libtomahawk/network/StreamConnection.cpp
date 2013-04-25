9aba1d6626233463d56d0400f1bc4579011b394c
AltarBeastiful <r3m1.benoit@gmail.com>
2013-04-25 20:49:04 +0200

    Add quality parameter for transcoding. Seek using Phonon when streaming a conversion. Conflicts: 	src/libtomahawk/network/StreamConnection.cpp

--- src/libtomahawk/network/StreamConnection.cpp
+++ src/libtomahawk/network/StreamConnection.cpp
@@ -30,6 +30,9 @@
 #include "database/Database.h"
 #include "SourceList.h"
 #include "utils/Logger.h"
+#include "utils/VorbisConverter.h"
+
+#define BITRATE_THRESHOLD 350
 
 using namespace Tomahawk;
 
@@ -183,7 +186,13 @@ StreamConnection::startSending( const Tomahawk::result_ptr& result )
 
     m_result = result;
 
-    m_result->setToConvert( (m_result->bitrate() >= 350) );
+    m_result->setToConvert( (m_result->bitrate() >= BITRATE_THRESHOLD) );
+    if( m_result->bitrate() >= BITRATE_THRESHOLD )
+    {
+        m_result->setBitrate(VorbisConverter::outputBitrate());
+        m_result->setMimetype("application/ogg");
+        m_result->setSize(VorbisConverter::convertedSize(m_result->track()->duration()));
+    }
 
     qDebug() << "Starting to transmit" << m_result->url();
 
--- src/libtomahawk/utils/VorbisConverter.cpp
+++ src/libtomahawk/utils/VorbisConverter.cpp
@@ -37,22 +37,21 @@
 
 using namespace Tomahawk;
 
-#define STANDARD_SAMPLE_RATE 44100
-#define DEFAULT_IS_STEREO true
-
-
 VorbisConverter::VorbisConverter(const Tomahawk::result_ptr& result, QObject *parent) :
     QIODevice(parent),
     m_remainingSamples(-1),
     m_buffer( new QByteArray() ),
-    m_atEnd( false )
+    m_atEnd( false ),
+    m_pos( 0 ),
+    m_outputBitrate( OUTPUT_BITRATE )
 {
-
     m_mediaObject = new Phonon::MediaObject( this );
     m_audioOutput = new Phonon::AudioOutput( Phonon::MusicCategory, this );
     m_audioDataOutput = new Phonon::AudioDataOutput( m_mediaObject );
-    m_stream =  new QBuffer(m_buffer);
+//    m_stream =  new QBuffer(m_buffer);
     m_vorbisWriter = new VorbisWriter(this);
+    //m_outputBitrate = quality > 0 && quality <= 10 ? quality * 45000 : DEFAULT_OUTPUT_BITRATE;
+    m_duration = result->track()->duration() * 1000;
 
     Phonon::createPath( m_mediaObject, m_audioDataOutput );
 
@@ -65,16 +64,16 @@ VorbisConverter::VorbisConverter(const Tomahawk::result_ptr& result, QObject *pa
              SLOT( receiveData( const QMap<Phonon::AudioDataOutput::Channel, QVector<qint16> >& ) ));
         connect( m_audioDataOutput, SIGNAL( endOfMedia(int) ), this, SLOT( onEndOfMedia(int) ) );
         connect( m_mediaObject, SIGNAL( stateChanged( Phonon::State, Phonon::State ) ), SLOT( onStateChanged( Phonon::State, Phonon::State ) ) );
+        connect( m_mediaObject, SIGNAL( totalTimeChanged(qint64) ), this, SLOT( onTotalTimeChanged(qint64) ) );
 
         m_vorbisWriter->addTag("ARTIST", result->track()->artist());
         m_vorbisWriter->addTag("ALBUM", result->track()->album());
         m_vorbisWriter->addTag("TITLE", result->track()->track());
 
-        tDebug() << "opening : " << m_mediaObject->currentSource().mrl().path() ;
-        test = new QFile(m_mediaObject->currentSource().mrl().path() + ".ogg");
-        test->open(ReadWrite);
-        m_stream->open(QBuffer::ReadWrite);
-        m_vorbisWriter->open( m_audioDataOutput->sampleRate(), DEFAULT_IS_STEREO , 10);
+        tDebug() << "opening : " << m_mediaObject->currentSource().mrl().path() << " with duration : " << result->track()->duration() ;
+        //m_stream->open(QBuffer::ReadWrite);
+
+        m_vorbisWriter->open( m_audioDataOutput->sampleRate(), DEFAULT_IS_STEREO , m_outputBitrate);
     }
 
     tDebug() << "Data size : " << m_audioDataOutput->dataSize() << " with bitrate : " << m_audioDataOutput->sampleRate();
@@ -87,7 +86,6 @@ VorbisConverter::startConversion()
 {
     tDebug() << "Playing : " << m_mediaObject->currentSource().url() << "with bitrate : " << m_audioDataOutput->sampleRate() ;
 
-    //m_audioOutput->setVolume(0);
     m_mediaObject->play();
 }
 
@@ -97,14 +95,13 @@ qint64 VorbisConverter::readData(char *data, qint64 maxSize)
     int length = qMin(m_buffer->length(), (int) maxSize);
     memcpy(data, m_buffer->constData(), length);
     m_buffer->remove(0, length);
-    //test->write(data, length);
+    m_pos += length;
     return length;
 }
 
 
 qint64 VorbisConverter::writeData(const char *data, qint64 maxSize)
 {
-    //test->write(data, maxSize);
     m_buffer->append(data, maxSize);
     return maxSize;
 }
@@ -113,8 +110,27 @@ qint64 VorbisConverter::writeData(const char *data, qint64 maxSize)
 bool
 VorbisConverter::seek ( qint64 pos )
 {
-    tDebug() << "Called seek for pos : " << pos << " result : " << false;
-    return false;
+    QIODevice::seek(pos);
+
+    qint64 timePosition = (pos / m_outputBitrate) * 1000;
+    tDebug() << "Called seek for pos : " << pos << " seeking time : " << timePosition << "ms phonon thinks : " << m_mediaObject->isSeekable();
+
+    if(!m_mediaObject->isSeekable() || timePosition > m_duration)
+    {
+        return false;
+    }
+
+    m_mediaObject->seek(timePosition);
+    m_pos = pos;
+    return true;
+}
+
+
+qint64
+VorbisConverter::bytesAvailable() const
+{
+    return ( m_duration / 1000 ) * m_outputBitrate;
+//    return m_buffer->size() + QIODevice::bytesAvailable();
 }
 
 
@@ -128,11 +144,6 @@ VorbisConverter::seek ( qint64 pos )
 //qint64
 //VorbisConverter::writeData(const char *data, qint64 maxSize)
 //{
-////    char* tmp = new char[maxSize];
-////    memcpy(tmp, data, maxSize);
-////    test->write(tmp, maxSize);
-////    return m_stream->write(tmp, maxSize);
-
 //    m_buffer->append(data, maxSize);
 //    return maxSize;
 //}
@@ -149,15 +160,14 @@ VorbisConverter::seek ( qint64 pos )
 
 qint64 VorbisConverter::pos () const
 {
-    return m_stream->pos();
+    return m_pos;
 }
 
 
-qint64
-VorbisConverter::bytesAvailable() const
-{
-    return m_buffer->size() + QIODevice::bytesAvailable();
-}
+//qint64 VorbisConverter::size() const
+//{
+//    return this->bytesAvailable();
+//}
 
 
 bool
@@ -168,16 +178,14 @@ VorbisConverter::atEnd () const
 
 
 void
-VorbisConverter::receiveData(const QMap<Phonon::AudioDataOutput::Channel , QVector<qint16> > &data)
+VorbisConverter::receiveData( const QMap<Phonon::AudioDataOutput::Channel , QVector<qint16> > &data )
 {
-    //tDebug() <<"Received Data from audio";
-
     QVector<qint16> left = data[Phonon::AudioDataOutput::LeftChannel];
     QVector<qint16> right = data[Phonon::AudioDataOutput::RightChannel];
     long sampleSize;
     bool flush = false;
 
-    if(m_remainingSamples == -1)
+    if( m_remainingSamples == -1 )
         sampleSize = left.size();
     else
     {
@@ -192,19 +200,18 @@ VorbisConverter::receiveData(const QMap<Phonon::AudioDataOutput::Channel , QVect
     }
     else
     {
-        qint64 streamSize = m_stream->size();
         int bufferSize = m_buffer->size();
-        m_vorbisWriter->write(left.constData(), right.constData(), sampleSize, flush);
-        if(streamSize != m_stream->size() && bufferSize != m_buffer->size())
+        m_vorbisWriter->write( left.constData(), right.constData(), sampleSize, flush) ;
+        if( bufferSize != m_buffer->size() )
         {
-            tDebug() << "AFTER : array size : "<<m_buffer->size()<<" stream size : " <<m_stream->size()<<" pos : "<<m_stream->pos();
+            tDebug() << "AFTER : array size : "<<m_buffer->size()<<" pos : "<<this->pos();
         }
     }
 }
 
 
 void
-VorbisConverter::onEndOfMedia(int remainingSamples)
+VorbisConverter::onEndOfMedia( int remainingSamples )
 {
     tDebug() << "End of media, with : " << remainingSamples ;
     m_remainingSamples = remainingSamples;
@@ -216,13 +223,19 @@ VorbisConverter::onStateChanged( Phonon::State newState, Phonon::State oldState 
 {
     tDebug() << "State changed : " << newState << " was " << oldState;
 
-    if(oldState == Phonon::PlayingState && newState == Phonon::StoppedState)
+    if( oldState == Phonon::PlayingState && newState == Phonon::StoppedState )
     {
         m_atEnd = true;
         m_vorbisWriter->close();
-        test->close();
     }
 
+}
+
+void
+VorbisConverter::onTotalTimeChanged( qint64 newTotalTime )
+{
+    tDebug() << "Old total time : " << m_duration << "New totla time : " << newTotalTime;
+    m_duration = newTotalTime;
 }
 
 
